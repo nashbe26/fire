6c17d67620138ae4b238ce261cdfa2580f3476a6
hatem782 <hatembenechikh100@gmail.com>
2023-07-24 10:01:58 +0100

    fix

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,19 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user.controller");
-const { validateCurrentUserUpdate } = require("../middleware/verifyTalent.midddleware");
+const {
+  validateCurrentUserUpdate,
+} = require("../middleware/verifyTalent.midddleware");
 
 const { uploadImage } = require("../utils/multer");
 const { uploadPDF } = require("../utils/multerCV");
 
-router.put("/updateUserData",validateCurrentUserUpdate, userController.updateUser);
+router.put(
+  "/updateUserData",
+  validateCurrentUserUpdate,
+  userController.updateUser
+);
 router.put("/updatePassword", userController.updatePasswordUser);
 router.get("/updateViews/:id", userController.updateUserViews);
 router.get("/getUser", userController.getUsers);
 router.get("/getUserById/:id", userController.getUserById);
-router.post("/updateUserImage",uploadImage ,userController.addUserPorfile);
-router.post("/uploadCV",uploadPDF ,userController.sendCV);
+router.post("/updateUserImage", uploadImage, userController.addUserPorfile);
+router.post("/uploadCV", uploadPDF, userController.sendCV);
 router.get("/sendToRec/:job_id", userController.sendToRec);
 router.delete("/deleteUser", userController.deleteUser);
 
-module.exports = router;    
+module.exports = router;
