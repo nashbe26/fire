402f4c61eebcf0d01aedb2a1bef40ed4c25a0563
hatem782 <hatembenechikh100@gmail.com>
2023-07-31 01:18:59 +0100

    fix

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -1,24 +1,23 @@
 const asyncHandler = require("express-async-handler");
 const notificationServices = require("../services/notification.service");
 
-
-const deleteNotification = asyncHandler(async(req,res)=> {
-    
-    const notificationId =req.params.notificationId;
-    let deleted = await notificationServices.deleteNotification(notificationId,req.user);
-    res.status(200).json({message : deleted})
-    
-})
-
-const getNotifications = asyncHandler(async(req,res)=> {
-    
-    let notifications = await notificationServices.getNotifications(req.user);
-    res.status(200).json({notifications : notifications});
+const deleteNotification = asyncHandler(async (req, res) => {
+  const notificationId = req.params.notificationId;
+  let deleted = await notificationServices.deleteNotification(
+    notificationId,
+    req.user
+  );
+  res.status(200).json({ message: deleted });
 });
 
-
+const getNotifications = asyncHandler(async (req, res) => {
+  let notifications = await notificationServices.getNotifications(
+    req.user.user._id
+  );
+  res.status(200).json({ notifications: notifications });
+});
 
 module.exports = {
-    deleteNotification,
-    getNotifications
-  };
+  deleteNotification,
+  getNotifications,
+};
